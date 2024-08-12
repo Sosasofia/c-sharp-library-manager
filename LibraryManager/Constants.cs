d52aff9456b98c3aeb6a24b112e8bebf28080ad5
Sosa Sofia <80438887+Sosasofia@users.noreply.github.com>
2024-08-12 00:08:15 -0300

    Fix book management

--- LibraryManager/Constants.cs
+++ LibraryManager/Constants.cs
@@ -2,9 +2,7 @@
 public static class Constants
 {
     public const string Title = "\tLibrary Manager\r";
-    public const string Separator = "----------------------------------------\n";
-    public const string Separator2 = "\t***********************************\n";
-
+    public const string Separator = "**************************************\n";
 
     public const string MenuMessage = "\tMenu: \n";
     public const string AddBook = "\t1 - Add book";
@@ -17,23 +15,26 @@ public static class Constants
     public const string LoanHistory = "\t8 - Loan history";
     public const string FinishExecution = "\t9 - Finish execution";
 
-
     public const string InvalidOption = "Invalid option!";
     public const string InvalidID = "Invalid ID";
+    public const string InvalidYear = "Invalid year range";
     public const string Error = "Error";
+    public const string ErrorMessage = "Error: {0}";
     public const string NotFound = "Not Found";
-    
+    public const string NoBooks = "There are no registered books";
+    public const string EmptyInput = "Input field can not be empty";
+    public const string NumberInput = "Input must be a number";
+    public const string BookNotFound = "Book with title *{0}* not found!";
+    public const string BookAlreadyExists = "Book title already exist";
+    public const string UserAlreadyExists = "Username already exists";
+    public const string AlredyExists = "Already exists {0}";
 
     public const string RequestTitle = "\tEnter book title: ";
     public const string RequestAuthor = "\tEnter book author: ";
     public const string RequestISBN = "\tEnter book ISBN: ";
     public const string RequestPublicationYear = "\tEnter book publication date: ";
     public const string RequestUserId = "\tEnter user id: ";
-
     public const string RequestUserName = "\tEnter username: ";
 
     public const string UserCreated = "\tSuccesfull user creation!";
-
-    public const string BookAlreadyExists = "Book title already exist";
-    public const string UserAlreadyExists = "Username already exists";
 }
